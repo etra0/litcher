d316205a740e262b3919a5c1861bbe47f41e8c98
Sebastián Aedo <sebastian.aedo29@gmail.com>
2022-10-13 00:36:48 -0300

    @@@: wip

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,11 @@
 
 use std::panic::PanicInfo;
 
+use anyhow::{Context, Result};
 use imgui::ColorEditFlags;
 use memory_rs::generate_aob_pattern;
 use memory_rs::internal::process_info::ProcessInfo;
 use windows_sys::Win32::UI::Input::KeyboardAndMouse::*;
-use anyhow::{Context, Result};
 
 use hudhook::hooks::dx11::ImguiDX11Hooks;
 use hudhook::hooks::{ImguiRenderLoop, ImguiRenderLoopFlags};
@@ -65,18 +65,45 @@ impl LitcherContext {
 
         let memory_pools = Self::find_memory_pools(&proc_info).unwrap();
 
-        // Another option:
-        // [$process + 0x2c5dd38]
-        // [0x1A8, 0x40] <= CR4Player
-        let player: Pointer<ScriptedEntity<EmptyVT>> = Pointer::new(
-            proc_info.region.start_address + 0x2c5dd38,
-            vec![0x1A8, 0x40],
-        );
+        let player: Pointer<ScriptedEntity<EmptyVT>> = {
+            let mp = generate_aob_pattern![
+                0x48, 0x8b, 0x0d, _, _, _, _, 0x48, 0x8b, 0x55, 0xc0, 0x48, 0x8b, 0x01, 0x48, 0x81,
+                0xc2, 0xa0, 0x00, 0x00, 0x00
+            ];
+
+            let instr = (proc_info
+                .region
+                .scan_aob(&mp)
+                .context("Couldn't find Player entity")
+                .unwrap()
+                .unwrap()) as *const u8;
+
+            let instruction_length = 7_usize;
+
+            let offset = unsafe { *(instr.offset(3) as *const u32) } as usize;
+
+            let player_entity = (instr as usize) + instruction_length + offset;
+
+            Pointer::new(player_entity, vec![0x1A8, 0x40])
+        };
 
         let lights = Vec::new();
 
-        let memory_pool_func: MemoryPoolFunc =
-            unsafe { std::mem::transmute(proc_info.region.start_address + 0x03c400) };
+        let memory_pool_func: MemoryPoolFunc = {
+            let mp = generate_aob_pattern![
+                0x48, 0x89, 0x5c, 0x24, 0x08, 0x48, 0x89, 0x74, 0x24, 0x10, 0x57, 0x48, 0x83, 0xec,
+                0x20, 0x49, 0x8b, 0xf1, 0x41, 0x0f, 0xb6, 0xd8, 0x48, 0x8b, 0xf9
+            ];
+
+            // TODO: Remove this ugly double unwrap.
+            let addr = proc_info
+                .region
+                .scan_aob(&mp)
+                .context("Couldn't find Memory Pool func")
+                .unwrap()
+                .unwrap();
+            unsafe { std::mem::transmute(addr) }
+        };
 
         Self {
             memory_pools,
@@ -93,16 +120,19 @@ impl LitcherContext {
     /// they have references to it. We can safely assume those instructions
     /// *will* exist in both GOG's and Steam's version, so we can avoid having
     /// hardcoded offsets.
-    fn find_memory_pools(proc_inf: &ProcessInfo) -> Result<MainMemoryPools> {
-        let region = &proc_inf.region;
+    fn find_memory_pools(proc_info: &ProcessInfo) -> Result<MainMemoryPools> {
+        let region = &proc_info.region;
         // Memory pattern for PointLightComponent memory pool
-        let mp = generate_aob_pattern![0x45, 0x32, 0xC9, 0x32, 0xD0, 0x80,
-        0xE2, 0x01, 0x32, 0xD1, 0x88, 0x93, 0x74, 0x01, 0x00, 0x00, 0x48, 0x8B,
-        0x05, _, _, _, _];
+        let mp = generate_aob_pattern![
+            0x45, 0x32, 0xC9, 0x32, 0xD0, 0x80, 0xE2, 0x01, 0x32, 0xD1, 0x88, 0x93, 0x74, 0x01,
+            0x00, 0x00, 0x48, 0x8B, 0x05, _, _, _, _
+        ];
 
         // The right instruction is in 0x10 offset from what we find.
-        let instr = (region.scan_aob(&mp)?
-            .context("Couldn't find the PointLight Memory Pool")? + 0x10) as *const u8;
+        let instr = (region
+            .scan_aob(&mp)?
+            .context("Couldn't find the PointLight Memory Pool")?
+            + 0x10) as *const u8;
 
         // Here we're supposed to do some trickery.
         // Basically, the `mov` instruction works with offsets, in this case,
@@ -118,17 +148,19 @@ impl LitcherContext {
 
         // Finally, the *real* address would be
         //   instr + instruction_length + offset
-        let point_light_memorypool =
-            (instr as usize) + instruction_length + offset;
+        let point_light_memorypool = (instr as usize) + instruction_length + offset;
 
         // Now we try to find the SpotLightComponent
 
-        let mp = generate_aob_pattern![0x0f, 0x84, 0x92, 0x00, 0x00, 0x00,
-        0x48, 0x8b, 0x1d, _, _, _, _, 0x48, 0x8d, 0x8c, 0x24, 0x30, 0x01, 0x00,
-        0x00];
+        let mp = generate_aob_pattern![
+            0x0f, 0x84, 0x92, 0x00, 0x00, 0x00, 0x48, 0x8b, 0x1d, _, _, _, _, 0x48, 0x8d, 0x8c,
+            0x24, 0x30, 0x01, 0x00, 0x00
+        ];
 
-        let instr = (region.scan_aob(&mp)?
-            .context("Couldn't find SpotLightComponent Memory Pool")? + 0x6) as *const u8;
+        let instr = (region
+            .scan_aob(&mp)?
+            .context("Couldn't find SpotLightComponent Memory Pool")?
+            + 0x6) as *const u8;
 
         let instruction_length = 7_usize;
 
@@ -138,7 +170,7 @@ impl LitcherContext {
 
         Ok(MainMemoryPools {
             spotlight: Pointer::new(spot_light_memorypool, Vec::new()),
-            pointlight: Pointer::new(point_light_memorypool, Vec::new())
+            pointlight: Pointer::new(point_light_memorypool, Vec::new()),
         })
     }
 
